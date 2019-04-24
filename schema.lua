522effb9b477e8dc0dfb7ca8274c6eac04757825
dongzhiyong <dongzhiyong@beisen.com>
2019-04-24 16:04:44 +0800

    modify: 修改post请求检测逻辑

--- schema.lua
+++ schema.lua
@@ -14,6 +14,7 @@ local function validate_ips(v, t, column)
 end
 
 return {
+  name = "kong-waf",
   fields = {
     whitelist = {type = "array", func = validate_ips},
     blacklist = {type = "array", func = validate_ips},
